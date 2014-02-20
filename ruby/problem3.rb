2620b16d71b587129e7124f37bd8cb2ea29f6057
Tyler Mauthe <me@tylermauthe.com>
2014-02-20 10:02:42 -0700

    Fix problem 3 using prime module
    Using ruby's prime module drastically simplified this solution.
    
    The underlying algorithm works as follows:
    
    - Iterate over a range of pseudoprimes generated by Prime::Generator23
    - Perform trial division on each pseudoprime, if its a factor then collect
      it
    - Return all collected factors as array
    - Take the largest number from 2d array

--- ruby/problem3.rb
+++ ruby/problem3.rb
@@ -1,35 +1,5 @@
+require 'prime'
+
 number = 600851475143
 
-#Heuristic to scale haystack size - only works for square root-smooth numbers
-def primeScalingFunction n
-  if (scaledNum = Math.sqrt(n).floor) > 2
-    return scaledNum
-  elsif (n/2) > 2
-    return n/2
-  else 
-    return n-1
-  end
-end
-
-#Determines primality of number
-def is_prime? n
-  isPrime = true
-
-  (2..primeScalingFunction(n)).each do |d|
-    if (n % d) == 0
-      isPrime = false
-    end
-  end
-  
-  return isPrime
-end
-
-#Find prime factors, starting with biggest
-primeScalingFunction(number-1).downto(2) do |term|
-  if (number%term) == 0
-    if is_prime?(term)
-      puts term
-      break
-    end
-  end
-end
+puts number.prime_division.max.max
