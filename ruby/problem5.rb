c620d63eaa3467e192dc7d01ec559e5f46d3ef8b
Tyler Mauthe <me@tylermauthe.com>
2014-02-20 12:52:29 -0700

    Proper solution of Problem 5

--- ruby/problem5.rb
+++ ruby/problem5.rb
@@ -2,21 +2,4 @@ UPPER_BOUND = 20
 
 range = 1..UPPER_BOUND
 
-numberToTest = UPPER_BOUND
-
-numberFound = false
-
-while !numberFound do
-  numberFound = true
-  range.each do |divisor|
-    if numberToTest%divisor != 0
-      numberFound = false
-      break
-    end
-  end
-  if !numberFound
-    numberToTest += 1
-  end
-end
-
-puts numberToTest
+puts range.reduce {|memo, num| memo.lcm(num)}
