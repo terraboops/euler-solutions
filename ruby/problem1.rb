95f6ce2e5e16e043ac38a0f566de644b5e6775d1
Tyler Mauthe <me@tylermauthe.com>
2014-02-20 01:52:48 -0700

    Add comments

--- ruby/problem1.rb
+++ ruby/problem1.rb
@@ -1,5 +1,7 @@
 sum = 0
+#iterate positive integers smaller than 1000
 (1...1000).each do |num|
+  #sum if divisible by 3 or 5 (3 is tested first because there are more factors -- short circuiting ftw)
   if num%3 == 0 or num%5 == 0
     sum = sum + num
   end
--- ruby/problem2.rb
+++ ruby/problem2.rb
@@ -1,8 +1,11 @@
+#Memoized fibonacci function - handles bigass numbers
 fib = Hash.new do |fibs, num|
   fibs[num] = 
     if num <= 2
+      #base case
       num
     else
+      #recursive case
       fibs[num-1] + fibs[num-2]
     end
 end
@@ -10,7 +13,9 @@ end
 sum = 0
 index = 0
 
+#Iterate until term is larger than 4,000,000
 while (term = fib[index += 1]) < 4000000 do
+  #sum if even
   if term.even?
     sum += term
   end
