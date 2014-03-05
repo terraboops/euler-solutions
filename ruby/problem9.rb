72bf81f5a31dede01bda2b5a0b914eb940fe8d85
Tyler Mauthe <me@tylermauthe.com>
2014-03-04 22:09:26 -0700

    Solve problem 9
    This was a fun one! Used linear algebra and the matrix module to create a
    tree of primitive pythagorean triples. Then the problem was simply a
    matter of searching the tree for a triple whose sum was equal to the
    needle or divided it evenly.
    
    This is crazy fast.

---
+++ ruby/problem9.rb
@@ -0,0 +1,58 @@
+require 'matrix'
+require 'benchmark'
+time = Benchmark.realtime do
+  VALUE_BEING_SEARCHED = 1000
+
+  # http://en.wikipedia.org/wiki/Tree_of_primitive_Pythagorean_triples
+  A = Matrix[
+              [1, -2, 2],
+              [2, -1, 2],
+              [2, -2, 3]
+            ]
+  C = Matrix[
+              [-1, 2, 2],
+              [-2, 1, 2],
+              [-2, 2, 3]
+            ]
+  B = Matrix[
+              [1, 2, 2],
+              [2, 1, 2],
+              [2, 2, 3]
+            ]
+
+  def cascade_primitive_triplets(root, matrix)
+    sum_abc = root[0,0] + root[1,0] + root[2, 0]
+    if(sum_abc == VALUE_BEING_SEARCHED or VALUE_BEING_SEARCHED % sum_abc == 0)
+      if(sum_abc != VALUE_BEING_SEARCHED)
+        scale_factor = VALUE_BEING_SEARCHED / sum_abc
+        root = root.map {|num| num * scale_factor}
+      end
+      return root
+    elsif(sum_abc < VALUE_BEING_SEARCHED)
+      next_root = matrix * root
+      traverse_each_matrix(next_root)
+    else
+      return false
+    end
+  end
+
+  def traverse_each_matrix(root)
+
+    value = cascade_primitive_triplets(root, A)
+    value = value ? value : cascade_primitive_triplets(root, C)
+    value = value ? value : cascade_primitive_triplets(root, B)
+
+    return value
+  end
+
+  # The smallest primitive triple
+  root = Matrix[[3],
+                [4],
+                [5]]
+
+  @result = traverse_each_matrix(root)
+end
+
+puts "A: #{@result[0,0]}, B: #{@result[1,0]}, C: #{@result[2,0]}"
+puts "product: #{@result[0,0]*@result[1,0]*@result[2,0]}"
+puts "Time elapsed #{time*1000} milliseconds"
